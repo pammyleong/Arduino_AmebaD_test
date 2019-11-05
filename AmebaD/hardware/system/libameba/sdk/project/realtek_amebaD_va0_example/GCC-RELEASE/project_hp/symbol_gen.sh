98283771b06023d22ca8affd0760bc834e7345c9
zhangzhenwu <zhangzhenwu@realtek-sg.com>
2019-11-05 18:48:19 +0800

    update_sdk-edit

--- AmebaD/hardware/system/libameba/sdk/project/realtek_amebaD_va0_example/GCC-RELEASE/project_hp/symbol_gen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-cd asdk/config
-
-./export_symbol.py > ../lib/rom_symbol_v01.txt
-./export_symbol_iar.py > ../lib/rom_symbol_v01_iar.icf
-
-cd -
--- AmebaD/hardware/system/libameba/sdk/project/realtek_amebaD_va0_example/GCC-RELEASE/project_lp/jlink_script/cm4_jlink.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\SEGGER\JLink_V634b\JLinkGDBServer.exe" -select USB -device Cortex-M33 -if swd -scriptfile AP2_KM4.JLinkScript -port 2335
