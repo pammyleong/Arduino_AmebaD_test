96a2999d55acc428c8101023793319bde31e1f97
zhangzhenwu <zhangzhenwu@realtek-sg.com>
2019-11-25 11:05:33 +0800

    update_sdk-v0.1.1_tool-v0.0.5

--- AmebaD/ameba_d_tools_windows/tools/windows/src/amebasize.c
+++ AmebaD/ameba_d_tools_windows/tools/windows/src/amebasize.c
@@ -16,31 +16,31 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
-	sprintf(filename, "%s\\ram_1.p.bin", argv[1]);
-	FILE* fp_bootloader = fopen(filename, "rb");
-	if (fp_bootloader != NULL) {
-		fseek(fp_bootloader, 0L, SEEK_END);
-		size = ftell(fp_bootloader);
-		fclose(fp_bootloader);
-		printf("bootloader %d\n", size);
+	sprintf(filename, "%s\\km0_boot_all.bin", argv[1]);
+	FILE* fp_bootloader_km0 = fopen(filename, "rb");
+	if (fp_bootloader_km0 != NULL) {
+		fseek(fp_bootloader_km0, 0L, SEEK_END);
+		size = ftell(fp_bootloader_km0);
+		fclose(fp_bootloader_km0);
+		printf("bootloader_km0 %d\n", size);
 	}
 
-	sprintf(filename, "%s\\ram_2.p.bin", argv[1]);
+	sprintf(filename, "%s\\km4_boot_all.bin", argv[1]);
+	FILE* fp_bootloader_km4 = fopen(filename, "rb");
+	if (fp_bootloader_km4 != NULL) {
+		fseek(fp_bootloader_km4, 0L, SEEK_END);
+		size = ftell(fp_bootloader_km4);
+		fclose(fp_bootloader_km4);
+		printf("bootloader_km0 %d\n", size);
+	}
+
+	sprintf(filename, "%s\\km0_km4_image2.bin", argv[1]);
 	FILE* fp_img2 = fopen(filename, "rb");
 	if (fp_img2 != NULL) {
 		fseek(fp_img2, 0L, SEEK_END);
 		size = ftell(fp_img2);
 		fclose(fp_img2);
 		printf("image2 %d\n", size);
-	}
-
-	sprintf(filename, "%s\\ram_3.p.bin", argv[1]);
-	FILE* fp_img3 = fopen(filename, "rb");
-	if (fp_img3 != NULL) {
-		fseek(fp_img3, 0L, SEEK_END);
-		size = ftell(fp_img3);
-		fclose(fp_img3);
-		printf("image3 %d\n", size);
 	}
 
 	return 0;
