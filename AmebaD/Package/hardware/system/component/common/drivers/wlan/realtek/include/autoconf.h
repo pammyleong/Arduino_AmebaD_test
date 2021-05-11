03fc93d089a6c6e6b8035616fe719d76fa5c32e6
zhangzhenwu <zhangzhenwu@realtek-sg.com>
2021-05-11 20:26:12 +0800

    update v3.0.9-t05

--- AmebaD/Package/hardware/system/component/common/drivers/wlan/realtek/include/autoconf.h
+++ AmebaD/Package/hardware/system/component/common/drivers/wlan/realtek/include/autoconf.h
@@ -862,12 +862,13 @@ extern unsigned int g_ap_sta_num;
 
 #define TIME_THRES	20
 
+// zzw
 /* 80211 - K V R */
-#define CONFIG_IEEE80211K
-#define CONFIG_LAYER2_ROAMING
+//#define CONFIG_IEEE80211K
+//#define CONFIG_LAYER2_ROAMING
 #ifdef CONFIG_LAYER2_ROAMING
-    #define CONFIG_RTW_WNM
-    #define CONFIG_IEEE80211R
+    //#define CONFIG_RTW_WNM
+    //#define CONFIG_IEEE80211R
 #endif
 
 #endif //WLANCONFIG_H
