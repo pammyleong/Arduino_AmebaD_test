290043f3acf1712f9b35171bcecf9e392dcce041
pammyleong <pammyleong@realtek-sg.com>
2022-09-30 16:23:16 +0800

    [AmebaPro2] RTSP API Update - Remove RTSP_Config function
    Verification:
    Compiled without error on AmebaPro2 with Arduino IDE

--- AmebaPro2/Package/hardware/cores/ambpro2/rtsp_drv.h
+++ AmebaPro2/Package/hardware/cores/ambpro2/rtsp_drv.h
@@ -8,15 +8,6 @@
 //#include "freertos_service.h"
 ////#include "rtsp/rtsp_api.h"
 
-
-#define CMD_RTSP2_GET_PARAMS			MM_MODULE_CMD(0x01)
-#define CMD_RTSP2_SET_STREAMMING		MM_MODULE_CMD(0x02)
-#define CMD_RTSP2_SELECT_STREAM 		MM_MODULE_CMD(0x03)
-#define CMD_RTSP2_SET_APPLY				MM_MODULE_CMD(0x0d)
-
-
-void RTSP_Config (uint32_t rtsp_fps, uint32_t rtsp_bps, int video_codec);
-
 data_content_t *RTSP_Init (void);
 
 int RTSP_Select_Stream (int arg);
@@ -25,7 +16,7 @@ int RTSP_Set_Apply (void);
 
 int RTSP_Set_Streaming      (int arg);
 
-int RTSP_Set_Params (void);
+int RTSP_Set_Params (uint32_t rtsp_fps, uint32_t rtsp_bps, int video_codec);
 
 data_content_t *RTSP_DeInit (data_content_t *ctx);
 
